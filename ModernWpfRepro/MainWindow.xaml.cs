523baa03e78d64756fb34af940d22f611490acb6
Reilly Wood <reilly.wood@canalyst.com>
2021-01-12 17:13:03 -0800

    Add project files.

---
+++ ModernWpfRepro/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernWpfRepro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string text = "";
+            for (int i = 0; i < 10; i++)
+            {
+                text += "Hello World" + Environment.NewLine;
+            }
+
+            TextBoxWithBadScrollbar.Text = text;
+        }
+
+    }
+}
